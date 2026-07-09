f1167c195ee2ca039385eae8ec3d373f87298ff4
Akmal <akmalthemalong@gmail.com>
2026-07-09 13:02:14 +0800

    feat: update

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
+$publicPath = __DIR__ . '/public';
 
-define('LARAVEL_START', microtime(true));
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
+);
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
-    require $maintenance;
+/**
+ * This file allows us to emulate Apache's "mod_rewrite" functionality from the
+ * built-in PHP web server. This provides a convenient way to test a Laravel
+ * application without having installed a "real" web server software here.
+ */
+if ($uri !== '/' && file_exists($publicPath . $uri)) {
+    return false;
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/vendor/autoload.php';
+require_once $publicPath . '/index.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
